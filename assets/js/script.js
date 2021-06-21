8a6d4af8c978ad4e6e41666869071c1ccf8950f3
Nazala <nazala5151@gmail.com>
2021-06-21 10:20:11 -0500

    answer check works first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,21 +8,21 @@ var showQuestion = document.querySelector("#show-question");
 var questionTitle = document.querySelector("#questionTitle");
 var choices = document.querySelector("#choices");
 var startScreen = document.querySelector("#start-screen");
-
+var answerCheck = document.querySelector("#answerCheck");
 
 var questions = [
     { question: "Inside which HTML element do we put the JavaScript to connect it to the HTML?", 
         answerChoices: ["<script>", "<js>", "<link>", "<javascript>"], 
-        correctAnswer: "<script>"} ,
+        correctAnswer: 0} ,
     { question: "Where is the correct place to insert a JavaScript in the HTML?", 
         answerChoices: ["The <body> section", "The <head> section", "The <html> section", "After the <!DOCTYPE html>"], 
-        correctAnswer: "The <body> section"} ,
+        correctAnswer: 0} ,
     { question: "How do you write 'Hello World' in an alert box?", 
         answerChoices: ["msg('Hello World');", "msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"], 
-        correctAnswer: "alert('Hello World');"} ,
+        correctAnswer: 3} ,
     { question: "Who invented JavaScript?", 
         answerChoices: ["Douglas Crockford", "Sheryl Sandberg", "Brendan Eich", "Gary Almes"] , 
-        correctAnswer: "Brendan Eich"}
+        correctAnswer: 2}
 ]
 
 
@@ -54,41 +54,42 @@ function displayQuestions() {
     console.log(questionTitle);
     var currentQuestion = (questions[questionIdx]);
     questionTitle.textContent = currentQuestion.question;
-    currentQuestion.answerChoices.forEach(element => {
-        console.log(element);
+    currentQuestion.answerChoices.forEach((element, index) => {
+        console.log(index);
         var currentAnswers = document.createElement("button");
-        console.log(currentAnswers);
+        currentAnswers.id = index;
         currentAnswers.textContent = element;
         choices.appendChild(currentAnswers);
         currentAnswers.style.display = "block";
         currentAnswers.style.margin = "10px";
         startScreen.style.display = "none";
-
+        currentAnswers.addEventListener("click", isAnswerCorrect);   
        
-    } );
-     
+    });
+} 
 
 
 
-  
-    // isAnswerCorrect();
-    // if statement 
-    // if questionIdx < questions.length
-  
-}
-
-function isAnswerCorrect() {
-    
-
-    if (answerChoices.value === 0) {
-        infobox.innerHTML = "Correct!";
+function isAnswerCorrect(event) { 
+    console.log(event.target.id);
+    if (questions[questionIdx].correctAnswer === +event.target.id) {
+        answerCheck.textContent = "Correct!";
+        
     } else {
-        infoBox.innerHTML = ("Incorrect. Answer was: + "questions[questionIdx].correctAnswer" ");
+        answerCheck.textContent = "Incorrect. Answer was: " + questions[questionIdx].answerChoices[questions[questionIdx].correctAnswer];
     }
     
     if (questionIdx < questions.length) {
-    questionIdx++;
-    displayQuestions();
+            var pauseseconds = 2;
+            var pause = setInterval(function() {
+                pauseseconds--
+                if (pauseseconds === 0) {
+                    questionIdx++;
+                    displayQuestions();
+                    clearInterval(pause)
+                }
+            }, 1000)
+      
     }
 
 }
@@ -101,4 +102,5 @@ function answerIsWrong() {
 
 startButton.addEventListener("click", displayQuestions);
 
-currentAnswers.addEventListener("click", isAnswerCorrect);   // event listener for isAnswerCorrect
+
+// event listener for isAnswerCorrect
